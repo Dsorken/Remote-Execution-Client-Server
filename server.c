f85ce624d417614bf6779e7550c14800e283f3b5
Dsorken <113465018+Dsorken@users.noreply.github.com>
2024-05-08 16:55:25 -0600

    Update server.c

--- server.c
+++ server.c
@@ -1,3 +1,13 @@
-int main() {
-    
+#include <stdio.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <pthread.h>
+
+int main(int argc, char *argv[]) {
+    //Create socket to listen on
+    //bind socket to ip and port (43434)
+    //for each connection create a thread to handle that connection
+    int socket_connection = socket()
+
+
 }
