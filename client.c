2809e638e51052abae306cb695415c0779eedeb2
Dsorken <113465018+Dsorken@users.noreply.github.com>
2024-05-08 19:48:15 -0600

    Create client.c

---
+++ client.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    struct sockaddr_in server_address;
+}   
